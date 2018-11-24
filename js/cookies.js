1e2f272acefbcbaca2f9c1689762b57cd5026fe2
MGenteluci <mgenteluci97@gmail.com>
2018-11-24 04:56:20 -0200

    remover cookies

--- js/cookies.js
+++ js/cookies.js
@@ -3,5 +3,11 @@ const criarCookieNome = nome => {
 }
 
 const removerCookies = () => {
+  let cookies = document.cookie
+                    .split('; ')
+                    .map(c => c.split('='));
 
+    cookies.forEach(c => {
+        document.cookie = `${c[0]}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+    });
 };
