a64c1ace7705a771c07272d96eb6c07fd00d7ec2
MGenteluci <mgenteluci97@gmail.com>
2019-04-27 10:10:00 -0300

    renomeia pasta

---
+++ apresentacao-maria/js/index.js
@@ -0,0 +1,6 @@
+const analyzeBtn = document.querySelector('.analyze');
+
+analyzeBtn.addEventListener('click', async() => {
+  const response = await axios.get('https://maria-ia.herokuapp.com/maria');
+  console.log(response);
+});
