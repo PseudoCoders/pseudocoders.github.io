2245f7509230aa8f6fb12931b6d8307043dfbce5
MGenteluci <mgenteluci97@gmail.com>
2019-04-27 11:39:05 -0300

    Adiciona emoções e intenção criminosa

--- apresentacao-maria/js/result.js
+++ apresentacao-maria/js/result.js
@@ -1,8 +1,25 @@
 const cardText = document.querySelector('.card-text');
+const emotionsText = document.querySelector('.emotions');
+const criminalIntentText = document.querySelector('.criminalIntent');
+const categoriesText = document.querySelector('.categories');
+
+function parseEmotions(emotions) {
+  const found = emotions.reduce(emotion => emotion + ' ', '');
+
+  return `Emoções encontradas: ${found}`;
+}
+
+function parseCriminalIntent(criminalIntent) {
+  return criminalIntent ?
+    'Possui intenção criminosa' :
+    'Não possui intenção criminosa';
+}
 
 (async() => {
   const response = await axios.get('https://maria-ia.herokuapp.com/maria');
   const res = response.data;
 
   cardText.textContent = res.tweet;
+  emotionsText = parseEmotions(res.result.emotions);
+  criminalIntentText = parseCriminalIntent(res.result.hasCriminalIntent);
 })();
