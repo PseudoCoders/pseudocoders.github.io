57e04bf2f18b1bbccffeafbdb78794fb802f1f1d
MGenteluci <mgenteluci97@gmail.com>
2019-04-27 10:47:17 -0300

    Realiza busca de dados

--- apresentacao-maria/js/result.js
+++ apresentacao-maria/js/result.js
@@ -2,5 +2,7 @@ const cardText = document.querySelector('.card-text');
 
 (async() => {
   const response = await axios.get('https://maria-ia.herokuapp.com/maria');
-  console.log(response.data);
+  const res = response.data;
+
+  cardText.textContent = res.tweet;
 })();
