91dc8e8033413538a976e4a57acd3eba3248ed5f
Lucas Sodré <lucas.sodre.teixeira@outlook.com>
2019-04-29 14:25:43 -0300

    Alterações de froent end para melhora na interação

--- apresentacao-maria/js/result.js
+++ apresentacao-maria/js/result.js
@@ -5,34 +5,34 @@ const categoriesText = document.querySelector('.categories');
 const sentimentText = document.querySelector('.sentiment');
 
 function parseCriminalCategories(categories) {
-  return `Intenções criminosas encontradas: ${categories.join(', ')}`;
+    return `• Intenções criminosas encontradas: ${categories.join(' • ')}`;
 }
 
 function parseEmotions(emotions) {
-  return `Emoções encontradas: ${emotions.join(', ')}`;
+    return emotions ? `Emoções encontradas: ${emotions.join('• ')}` : '';
 }
 
 function parseCriminalIntent(criminalIntent) {
-  return criminalIntent ?
-    'Possui intenção criminosa' :
-    '';
+    return criminalIntent ?
+        '• Possui intenção criminosa' :
+        '';
 }
 
 function parseSentiment(sentiment) {
-  return sentiment ? `Tweet contém: ${sentiment}` : '';
+    return sentiment ? `• Tweet contém: ${sentiment}` : '';
 }
 
 (async() => {
-  const response = await axios.get('https://maria-ia.herokuapp.com/maria');
-  const res = response.data;
+    const response = await axios.get('https://maria-ia.herokuapp.com/maria');
+    const res = response.data;
 
-  console.log('response: ', res);
-  cardText.textContent = res.tweet;
-  emotionsText.textContent = parseEmotions(res.result.emotions);
-  criminalIntentText.textContent = parseCriminalIntent(res.result.hasCriminalIntent);
-  sentimentText.textContent = parseSentiment(res.result.sentiments);
+    console.log('response: ', res);
+    cardText.textContent = res.tweet;
+    emotionsText.textContent = parseEmotions(res.result.emotions);
+    criminalIntentText.textContent = parseCriminalIntent(res.result.hasCriminalIntent);
+    sentimentText.textContent = parseSentiment(res.result.sentiments);
 
-  if (res.result.hasCriminalIntent) {
-    categoriesText.textContent = parseCriminalCategories(res.result.criminalCategories);
-  }
+    if (res.result.hasCriminalIntent) {
+        categoriesText.textContent = parseCriminalCategories(res.result.criminalCategories);
+    }
 })();
