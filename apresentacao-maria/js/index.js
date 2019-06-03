331f7196a9ea89e97a47040150da2ddf725707b6
Lucas Sodré <lucas.sodre.teixeira@outlook.com>
2019-06-02 22:51:59 -0300

    Inserção de links para acessar tweets pré analisados e classificados como possível intenção criminosa e analisados em tempo real

--- apresentacao-maria/js/index.js
+++ apresentacao-maria/js/index.js
@@ -1,3 +1,5 @@
 const analyzeBtn = document.querySelector('.analyze');
+const criminalIntentBtn = document.querySelector('.criminalIntent');
 
+criminalIntentBtn.addEventListener('click', () => window.location.href = 'resultsPresentation.html');
 analyzeBtn.addEventListener('click', () => window.location.href = 'result.html');
